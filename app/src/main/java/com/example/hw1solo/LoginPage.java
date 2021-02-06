6f58a725b2d0e5a408f9dd5cf9693da9ba7f2fac
Jim Cabrera <77947717+J-Cabrera-Osornio@users.noreply.github.com>
2021-02-05 21:02:34 -0800

    Added Error page

--- app/src/main/java/com/example/hw1solo/LoginPage.java
+++ app/src/main/java/com/example/hw1solo/LoginPage.java
@@ -9,12 +9,27 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginPage extends AppCompatActivity {
 
+    private TextView textViewResult;
+
+    private String content = "";
     private EditText userNameInput;
     private EditText passWordEnter;
     private Button verifyCredentials;
+
+
 
 
     @Override
@@ -23,9 +38,11 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.login_page);
 
 
-
+        textViewResult = findViewById(R.id.text_view_result);
         userNameInput = (EditText) findViewById(R.id.userNameInput);
         String username = userNameInput.getText().toString();
+
+
 
         passWordEnter = (EditText) findViewById(R.id.passWordEnter);
         String verify = passWordEnter.getText().toString();
@@ -34,6 +51,40 @@ public class LoginPage extends AppCompatActivity {
         passWordEnter.addTextChangedListener(checkBlank);
 
         verifyCredentials = (Button) findViewById(R.id.verifyCredentials);
+
+
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    //textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Post> posts = response.body();
+                for (Post post : posts) {
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+                   textViewResult.append(content);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                //textViewResult.setText(t.getMessage());
+            }
+        });
+
 
         verifyCredentials.setOnClickListener(new View.OnClickListener() {
             @Override
